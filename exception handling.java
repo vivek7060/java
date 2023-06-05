e68209736433633b38f7639b9869f7f53f326334
Vivek Tomar <101477595+vivek7060@users.noreply.github.com>
2023-06-05 04:39:25 -0400

    Add files via upload

---
+++ exception handling.java
@@ -0,0 +1,12 @@
+class test{
+    public static void main(String args[])
+
+    {
+
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println(100/0);
+        System.out.println("5");
+    }
+}
